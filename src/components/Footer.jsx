f9f58fb5c3126a039a756e28cb3c2afbdd515a0c
alberto <ferrandezmirallesalberto@gmail.com>
2022-11-29 19:09:07 +0100

    #cambios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <footer className='w-4/5 md:w-3/5 pt-[10vh]'>
             <div className='flex justify-between items-center'>
                 <div><h1>Hecho por: ALBERTO FERRANDEZ MIRALLES</h1></div>
-                <div><FaGithub size={20}/></div>
+                <div><a href="https://github.com/albertoferrandez" target="_blank"><FaGithub size={20}/></a></div>
             </div>
         </footer>
     )
