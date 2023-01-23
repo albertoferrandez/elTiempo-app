8d60b48a74f68adf809f41d8908b4161038334ae
alberto <ferrandezmirallesalberto@gmail.com>
2023-01-23 15:33:46 +0100

    mejora en la componetización

--- src/components/MostrarDatos.jsx
+++ src/components/MostrarDatos.jsx
@@ -1,58 +1,41 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import { getClima } from '../../services/getClima';
 import DatosCiudad from './DatosCiudad';
-import IntroCiudad from './IntroCiudad';
+import SearchCity from './SearchCity';
 
 const MostrarDatos = () => {
 
-  const [datos, setDatos] = useState({});
+    const [datos, setDatos] = useState({});
 
-  useEffect(() => {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=Vinaros&appid=36c85aaf32df1a52eb9a0e08218adb7f&lang={es}&units=metric"
-    ).then((response) => response.json())
-      .then((data) => setDatos(data))
-      .catch((error) => {
-        console.log(error)
-      });
-  }, [])
+    const searchCity = async (city) => {
+        
+        const getData = await getClima(city)
+        setDatos(getData)
+    }
 
-  const handleenviarCiudad = (location) => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=36c85aaf32df1a52eb9a0e08218adb7f&lang={es}&units=metric`
-    ).then((response) => response.json())
-      .then((data) =>setDatos(data))
-      .catch((error) => {
-        console.log(error)
-      })
-  }
+    return (
+        <>
+            <div className='w-5/6 lg:w-3/6 px-2 backdrop-blur-sm bg-white/30 rounded-md'>
+                <SearchCity handleNewCity={(e) => searchCity(e)} />
+            </div>
 
-  return (
-    <section className='MostrarDatos w-4/5 h-auto py-6 md:w-3/5 rounded-md
-    
-    backdrop-blur-md text-white'>
+            {
+                <DatosCiudad
+                    name={datos.name}
+                    pais={datos.main ? datos.sys.country : null}
+                    temp={datos.main ? Math.round(datos.main.temp) : null}
+                    main={datos.weather ? datos.weather[0].main : null}
+                    feel={datos.main ? Math.round(datos.main.feels_like) : null}
+                    max={datos.main ? Math.round(datos.main.temp_max) : null}
+                    min={datos.main ? Math.round(datos.main.temp_min) : null}
+                    hum={datos.main ? datos.main.humidity : null}
+                    viento={datos.main ? datos.wind.speed : null}
+                    lluvia={datos.main ? datos.main.humidity : null}
+                />
+            }
+        </>
 
-      <nav className='flex justify-center inline-block'>
-        <IntroCiudad enviarCiudad={handleenviarCiudad} />
-      </nav>
-
-      {
-        location === "" ? "" :
-          <DatosCiudad
-            name={datos.name}
-            pais={datos.main ? datos.sys.country : null}
-            temp={datos.main ? Math.round(datos.main.temp) : null}
-            main={datos.weather ? datos.weather[0].main : null}
-            feel={datos.main ? Math.round(datos.main.feels_like) : null}
-            max={datos.main ? Math.round(datos.main.temp_max) : null}
-            min={datos.main ? Math.round(datos.main.temp_min) : null}
-            hum={datos.main ? datos.main.humidity : null}
-            viento={datos.main ? datos.wind.speed : null}
-            lluvia={datos.main ? datos.main.humidity : null}
-          />
-      }
-
-    </section>
-  )
+    )
 }
 
 export default MostrarDatos
---
+++ src/components/SearchCity.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+import { MdSearch } from "react-icons/md";
+
+
+const SearchCity = ({ handleNewCity }) => {
+
+    const [city, setCity] = useState('')
+
+    const handleInputChange = ({ target }) => {
+        setCity(target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        setCity('')
+
+        handleNewCity(city)
+
+    }
+
+    return (
+        <form onSubmit={(event) => handleSubmit(event)} className="flex justify-between">
+            <input
+                value={city}
+                onChange={(e) => handleInputChange(e)}
+                placeholder='Introduce una ciudad . . .'
+                type='text'
+                className='bg-transparent border-none 
+                    focus:outline-none text-base py-3 w-full
+                    placeholder-black'
+                required
+            />
+            <button type='submit' className='px-2 py-2  transform hover:scale-110 motion-reduce:transform-none'><MdSearch size='20px' /></button>
+        </form>
+    )
+}
+
+export default SearchCity
+
+SearchCity.propTypes = {
+    handleNewCity: PropTypes.func.isRequired
+}
